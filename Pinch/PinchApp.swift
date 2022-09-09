761f54bc52b04095e0dcb94cb7e5af5135445eab
Max Herrera <harvey.herrera@lumation.co>
2022-09-09 12:43:08 -0600

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Max Herrera on 9/9/22.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
